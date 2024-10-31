9be424c01930d063098a61a5724f6fcc73442a62
civilianjoseph <jose_alm2013@hotmail.com>
2024-10-31 09:15:16 -0300

    aulas python

---
+++ Fatiamento strings.py
@@ -0,0 +1,12 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1]) # :-1 retorna o valor str ao contrário, pois começa a contagem do último caractere.
+#Contando da esquerda para a direita, 0 é sempre o primeiro caractere.
